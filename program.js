47f2de38c5bb79600acd60e78a71110b3d15278b
Krzysztof Ługowski <k.lugowski@yahoo.com>
2018-01-10 20:21:26 +0100

    Add files

---
+++ program.js
@@ -0,0 +1,26 @@
+process.stdin.setEncoding("utf-8");
+console.log("To check the current verion, type: version. To check the language, type: language. To exit, type: exit");
+
+process.stdin.on("readable", function() {
+	var input = process.stdin.read();
+	if (input !== null) {
+		var instruction = input.toString().trim();
+		switch (instruction) {
+			case "exit":
+				process.stdout.write("Quiting\n");
+				process.exit();
+			
+			case "version":
+				console.log(process.versions);
+				break;
+
+			case "language":
+				console.log(process.env);
+				break;
+
+			default:
+				process.stderr.write("Wrong instruction!!!\n");
+		}
+			
+	}
+});
